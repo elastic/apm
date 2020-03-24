9e00b265853b637d400235a21680bd81f077bbd2
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-24 23:24:56 +0100

    chore: remove the wildcard flag for Mac

--- scripts/download_gherkin_features.sh
+++ scripts/download_gherkin_features.sh
@@ -4,13 +4,18 @@ set -ex
 
 download_gherkin()
 {
+    wildcardFlag="--wildcards"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        wildcardFlag=""
+    fi
+
     rm -rf ${1} && mkdir -p ${1}
     for run in 1 2 3 4 5
     do
         if [ -x "$(command -v gtar)" ]; then
-            curl --silent --fail https://codeload.github.com/elastic/apm/tar.gz/${2} | gtar xzvf - --wildcards --directory=${1} --strip-components=4 "*/tests/agents/gherkin-specs*"
+            curl --silent --fail https://codeload.github.com/elastic/apm/tar.gz/${2} | gtar xzvf - ${wildcardFlag} --directory=${1} --strip-components=4 "*/tests/agents/gherkin-specs*"
         else
-            curl --silent --fail https://codeload.github.com/elastic/apm/tar.gz/${2} | tar xzvf - --wildcards --directory=${1} --strip-components=4 "*tests/agents/gherkin-specs*"
+            curl --silent --fail https://codeload.github.com/elastic/apm/tar.gz/${2} | tar xzvf - ${wildcardFlag} --directory=${1} --strip-components=4 "*tests/agents/gherkin-specs*"
         fi
         result=$?
         if [ $result -eq 0 ]; then break; fi
