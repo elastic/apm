c62df029c3f53ff5f01b2f886b1bb1fb8fc04f7c
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-26 08:26:58 +0100

    ci: fix hub path

--- .ci/scripts/install-dependencies.sh
+++ .ci/scripts/install-dependencies.sh
@@ -3,9 +3,9 @@
 set -uexo pipefail
 
 readonly HUB_VERSION="2.14.2"
-readonly HUB_CMD="${HOME}/bin/hub"
+readonly HUB_CMD="${HOME}/bin"
 
 # install GitHub's hub
 curl -fsSL https://github.com/github/hub/raw/master/script/get | bash -s ${HUB_VERSION}
 mkdir -p ${HUB_CMD}
-mv bin/hub ${HUB_CMD}
+mv bin/hub "${HUB_CMD}/hub"
