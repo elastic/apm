44f84eaac8e83ec631186766c3253faf6406deb2
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-26 23:02:06 +0100

    ci: refine messages

--- .ci/scripts/send-pr.sh
+++ .ci/scripts/send-pr.sh
@@ -11,12 +11,11 @@ readonly APM_AGENT_REPO_DIR="${GIT_DIR}/${APM_AGENT_REPO_NAME}"
 git clone "https://github.com/elastic/${APM_AGENT_REPO_NAME}" "${APM_AGENT_REPO_DIR}"
 
 mkdir -p "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
+echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
 cp tests/agents/gherkin-specs/*.feature "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
 
 cd ${APM_AGENT_REPO_DIR}
 git checkout -b update-feature-files-$(date "+%Y%m%d%H%M%S")
-echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
-git status
 git add ${APM_AGENT_SPECS_DIR}
 git commit -m "test: synchronizing bdd specs"
 
