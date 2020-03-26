8da3a39107e7e09be9e45bf555a1ed7689c297d8
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-26 09:25:26 +0100

    ci: improve send-pr script using HUB

--- .ci/scripts/send-pr.sh
+++ .ci/scripts/send-pr.sh
@@ -3,5 +3,29 @@
 set -uexo pipefail
 
 readonly APM_AGENT=${1}
+readonly APM_AGENT_SPECS_DIR=${2}
+readonly APM_AGENT_REPO_NAME="apm-agent-${APM_AGENT}"
 
-echo "Sending PR to apm-$APM_AGENT-agent"
+mkdir -p .ci/git
+git clone https://github.com/elastic/${APM_AGENT_REPO_NAME} .ci/git/${APM_AGENT_REPO_NAME}
+
+mkdir -p .ci/git/${APM_AGENT_REPO_NAME}/${APM_AGENT_SPECS_DIR}
+cp tests/agents/gherkin-specs/*.feature .ci/git/${APM_AGENT_REPO_NAME}/${APM_AGENT_SPECS_DIR}
+
+cd .ci/git/${APM_AGENT_REPO_NAME}
+git checkout -b update-feature-files-$(date "+%Y%m%d%H%M%S")
+echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
+git status
+git add ${APM_AGENT_SPECS_DIR}
+git commit -m "ci: Synchronized BDD specs"
+
+if [[ "${DO_SEND_PR}" == "true" ]]; then
+    hub pull-request \ 
+        -p \                                  # push the branch to the remote
+        -b master \                           # target branch 
+        --labels automation \                 # comma-separated list of tags
+        --reviewer @elastic/apm-agents \      # set agents as reviewer of the PR
+        -m ":robot: Synchronizing BDD specs"  # PR message
+else 
+    echo "PR sent to ${APM_AGENT_REPO_NAME}"
+fi
