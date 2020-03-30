625370326df5dcc49029ee459100b0d036382d1e
Manuel de la Peña <mdelapenya@gmail.com>
2020-03-30 23:10:41 +0200

    ci(jenkins): Automation for sending PRs to agents when changing the BDD specs (#231)
    * ci: first iteration to send a PR to apm agents
    
    We are not checking that the specs are modified, and the script is a dummy one
    
    * ci: use a build agent in the stage
    
    This will enable parallel execution of that stage
    
    * ci: remove when clause
    
    * ci: perform checkout in the proper dir
    
    * ci: support sending PRs to the subscribed agents in parallel
    
    * ci: install HUB in the build agent
    
    * ci: remove leftovers from template pipeline
    
    * ci: improve build agents usage
    
    Co-Authored-By: Victor Martinez <victormartinezrubio@gmail.com>
    
    * ci: refine environment variables definition
    
    Co-Authored-By: Victor Martinez <victormartinezrubio@gmail.com>
    
    * ci: fix hub path
    
    * ci: use gitCheckout build step
    
    * ci: subscribe more agents
    
    * ci: fixed BASE_DIR for parallel execution
    
    * ci: simplify stage name
    
    * ci: support a parameter to send the pr, false by default
    
    * ci: improve send-pr script using HUB
    
    * ci: refine pipeline after review
    
    Co-Authored-By: Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
    
    * ci: simplify git dir calculation
    
    Co-Authored-By: Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
    
    * ci: fix typo in env var
    
    * ci: quote values and remove non-needed mkdir
    
    * ci: use conventional commits
    
    As seen here: https://www.conventionalcommits.org/en/v1.0.0/
    
    Co-Authored-By: Victor Martinez <victormartinezrubio@gmail.com>
    
    * ci: use default branch as target of the PR
    
    * ci: refine messages
    
    * ci: set default git user in the build agent
    
    * ci: send the PRs if and only if there are changes in the gherkin specs
    
    * ci: indent block properly
    
    * ci: fix github reviewer
    
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>
    Co-authored-by: Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>

---
+++ .ci/scripts/send-pr.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -uexo pipefail
+
+readonly APM_AGENT=${1}
+readonly APM_AGENT_SPECS_DIR=${2}
+readonly APM_AGENT_REPO_NAME="apm-agent-${APM_AGENT}"
+readonly GIT_DIR=".ci/git"
+readonly APM_AGENT_REPO_DIR="${GIT_DIR}/${APM_AGENT_REPO_NAME}"
+
+git clone "https://github.com/elastic/${APM_AGENT_REPO_NAME}" "${APM_AGENT_REPO_DIR}"
+
+mkdir -p "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
+echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
+cp tests/agents/gherkin-specs/*.feature "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
+
+cd ${APM_AGENT_REPO_DIR}
+git checkout -b update-feature-files-$(date "+%Y%m%d%H%M%S")
+git add ${APM_AGENT_SPECS_DIR}
+git commit -m "test: synchronizing bdd specs"
+
+if [[ "${DO_SEND_PR}" == "true" ]]; then
+    hub pull-request \ 
+        -p \                                  # push the branch to the remote
+        --labels automation \                 # comma-separated list of tags
+        --reviewer @elastic/apm-agent-devs \  # set agents as reviewer of the PR
+        -m "test: synchronizing bdd specs"  # PR message
+else 
+    echo "PR sent to ${APM_AGENT_REPO_NAME}"
+fi
