c58ac347ff30478ed0a38df61d7d11d3a9c66b67
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-04-13 16:39:29 +0100

    ci(jenkins): use githubCreatePullRequest step

--- .ci/scripts/send-pr.sh
+++ .ci/scripts/send-pr.sh
@@ -4,12 +4,10 @@ set -uexo pipefail
 
 readonly APM_AGENT=${1}
 readonly APM_AGENT_SPECS_DIR=${2}
-readonly DO_SEND_PR=${3:-"true"}
 readonly APM_AGENT_REPO_NAME="apm-agent-${APM_AGENT}"
-readonly GIT_DIR=".ci/git"
-readonly APM_AGENT_REPO_DIR="${GIT_DIR}/${APM_AGENT_REPO_NAME}"
+readonly APM_AGENT_REPO_DIR=".ci/git/${APM_AGENT_REPO_NAME}"
 
-git clone "https://${GITHUB_TOKEN}@github.com/elastic/${APM_AGENT_REPO_NAME}" "${APM_AGENT_REPO_DIR}"
+git clone "https://github.com/elastic/${APM_AGENT_REPO_NAME}" "${APM_AGENT_REPO_DIR}"
 
 mkdir -p "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
 echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
@@ -19,9 +17,3 @@ cd ${APM_AGENT_REPO_DIR}
 git checkout -b update-feature-files-$(date "+%Y%m%d%H%M%S")
 git add ${APM_AGENT_SPECS_DIR}
 git commit -m "test: synchronizing bdd specs"
-
-if [[ "${DO_SEND_PR}" == "true" ]]; then
-    hub pull-request -p --labels automation -m "test: synchronizing bdd specs"
-else 
-    echo "PR sent to ${APM_AGENT_REPO_NAME}"
-fi
