14f82718bf08b83c5e344e63d3c02c31bc05e1fb
Victor Martinez <victormartinezrubio@gmail.com>
2020-04-17 19:04:07 +0100

    ci(jenkins): support githubCreatePullRequest step (#249)

---
+++ .ci/scripts/prepare-spec-changes.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -uexo pipefail
+
+readonly APM_AGENT_REPO_NAME=${1}
+readonly APM_AGENT_SPECS_DIR=${2}
+readonly APM_AGENT_REPO_DIR=".ci/${APM_AGENT_REPO_NAME}"
+
+git clone "https://github.com/elastic/${APM_AGENT_REPO_NAME}" "${APM_AGENT_REPO_DIR}"
+
+mkdir -p "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
+echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
+cp tests/agents/gherkin-specs/*.feature "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
+
+cd "${APM_AGENT_REPO_DIR}"
+git checkout -b "update-feature-files-$(date "+%Y%m%d%H%M%S")"
+git add "${APM_AGENT_SPECS_DIR}"
+git commit -m "test: synchronizing bdd specs"
