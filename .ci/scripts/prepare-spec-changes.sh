97defebb938065dd1653d15b2ead317f3efc2ca3
Manuel de la Peña <mdelapenya@gmail.com>
2020-08-13 13:23:43 +0200

    chore: prepare script for abstracting the file type
    Currently supporting gherkin, only

--- .ci/scripts/prepare-spec-changes.sh
+++ .ci/scripts/prepare-spec-changes.sh
@@ -3,16 +3,19 @@
 set -uexo pipefail
 
 readonly APM_AGENT_REPO_NAME=${1}
-readonly APM_AGENT_SPECS_DIR=${2}
+readonly SPECS_TYPE=${2} # gherkin
+readonly APM_AGENT_SPECS_DIR=${3}
 readonly APM_AGENT_REPO_DIR=".ci/${APM_AGENT_REPO_NAME}"
+
+EXTENSION="feature"
 
 git clone "https://github.com/elastic/${APM_AGENT_REPO_NAME}" "${APM_AGENT_REPO_DIR}"
 
 mkdir -p "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
-echo "Copying feature files to the ${APM_AGENT_REPO_NAME} repo"
-cp tests/agents/gherkin-specs/*.feature "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
+echo "Copying spec files to the ${APM_AGENT_REPO_NAME} repo"
+cp tests/agents/${SPECS_TYPE}-specs/*.${EXTENSION} "${APM_AGENT_REPO_DIR}/${APM_AGENT_SPECS_DIR}"
 
 cd "${APM_AGENT_REPO_DIR}"
-git checkout -b "update-feature-files-$(date "+%Y%m%d%H%M%S")"
+git checkout -b "update-spec-files-$(date "+%Y%m%d%H%M%S")"
 git add "${APM_AGENT_SPECS_DIR}"
-git commit -m "test: synchronizing bdd specs"
+git commit -m "test: synchronizing ${SPECS_TYPE} specs"
